ab756ca6fc0c116d27672693e6e059af7fb5ea19
nadila2k <nadilanawod@gmail.com>
2024-09-28 16:40:56 +0530

    getAdminChart query done

--- controller/analystController.js
+++ controller/analystController.js
@@ -41,9 +41,39 @@ const getFarmerAnalyst = async (req, res) => {
 };
 
 const getAdminChart = async (req, res) => {
+  let {year, progressId} = req.query;
 
-  
-console.log("admin cart")
+  year = year || new Date().getFullYear();
+  progressId = progressId || 1;
+
+  const [results, metadata] = await sequelize.query(`
+    SELECT 
+      max(y.id) AS "year_id", 
+      max(y.year) AS "year",
+      max(cs.weight) AS "year_weight_estimate",
+      max(cs.production) AS "per_perch_production",
+      max(cs."cropsId") AS "crops_id",
+      max(c.name) AS "crops_name",
+      max(fs.perch) AS "perch_count",
+      max(fs."progressId") AS "progress",
+      max(fs."userId") AS "farmer_id",
+      sum(cs.production * fs.perch) AS "farmer_monthly_production"
+    FROM public.years y
+    LEFT JOIN public."cropsStatistics" cs ON cs."yearId" = y.id
+    LEFT JOIN crops c ON c.id = cs."cropsId"
+    LEFT JOIN public."farmerStatistics" fs ON fs."cropsStatisticId" = cs.id
+    LEFT JOIN months m ON fs."monthId" = m.id
+    WHERE y.year=${year}  AND fs."progressId"=${progressId}
+    GROUP BY cs."cropsId"
+  `);
+
+  res.status(200).json({
+    status: "success",
+    message: "Data fetched",
+    data: {
+      data: results
+    }
+  })
 };
 
 const getFarmerChartId = async (req, res) => {
